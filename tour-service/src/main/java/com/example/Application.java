960f7a17f23c1ff5c0f5e24ad68b625bb2690911
anastasiaRC02 <anastasia.reed-comeaux@mastercard.com>
2025-01-07 18:34:11 +0000

    Created new tour in application.java

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -2,7 +2,7 @@ package com.example;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+import com.example.tourservice.TourManagementService;
 import com.example.tourservice.TravelAgentService;
 
 public class Application {
@@ -11,6 +11,9 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        TourManagementService tourManager = context.getBean(TourManagementService.class);
+        tourManager.createTour("Zoo Tour", 100, true);
+
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
